39035a604788b9258ac6e26f58fb822cefa8c405
MadhanKumarR150896 <madhankumar150896@outlook.com>
2026-01-17 22:05:53 +0530

    Backend integration and DAG validation

---
+++ backend_VS/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+import networkx as nx
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["Content-Type"]
+)
+
+@app.get('/')
+def read_root():
+    return {'Ping': 'Pong'}
+
+@app.post('/pipelines/parse')
+def parse_pipeline(pipeline: dict = Body(...)):
+
+    nodes = pipeline.get('nodes', [])
+    edges = pipeline.get('edges', [])
+
+    G = nx.DiGraph()
+
+    for node in nodes:
+        G.add_node(node.get('id'))
+
+    for edge in edges:
+        G.add_edge(edge.get('source'),edge.get('target'))
+    is_dag = nx.is_directed_acyclic_graph(G)
+
+    print(is_dag)
+    return {
+        "num_nodes":len(nodes),
+        "num_edges":len(edges),
+        "is_dag": is_dag
+    }
--- frontend_VS/src/components/submit.jsx
+++ frontend_VS/src/components/submit.jsx
@@ -1,4 +1,34 @@
+import { useStore } from "../utils/store";
+import { useShallow } from "zustand/shallow";
+
+const selector = (state) => ({
+  nodes: state.nodes,
+  edges: state.edges,
+});
+
 export const SubmitButton = () => {
+  const { nodes, edges } = useStore(useShallow(selector));
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  async function sendPipeline() {
+    try {
+      const response = await fetch(`${API_URL}/pipelines/parse`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nodes, edges }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(
+        "Couldn't provide the required data, please check the request",
+        error
+      );
+    }
+  }
+
   return (
     <div
       style={{
@@ -7,7 +37,11 @@ export const SubmitButton = () => {
         justifyContent: "center",
       }}
     >
-      <button style={{ backgroundColor: "#000", color: "#fff" }} type="submit">
+      <button
+        onClick={sendPipeline}
+        style={{ backgroundColor: "#000", color: "#fff" }}
+        type="submit"
+      >
         Submit
       </button>
     </div>
